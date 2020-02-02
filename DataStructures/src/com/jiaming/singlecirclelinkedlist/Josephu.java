cd539d874e688f75d1ca344804ce2106d294a0c8
tom.glb <goodMorning_glb@jiaming.com>
2020-02-02 19:18:53 +0800

    Josephu环

--- DataStructures/src/com/jiaming/singlecirclelinkedlist/Josephu.java
+++ DataStructures/src/com/jiaming/singlecirclelinkedlist/Josephu.java
@@ -4,7 +4,7 @@ package com.jiaming.singlecirclelinkedlist;
  * @author jmstart
  * @create 2020-02-01 17:18
  */
-public class Josepfu {
+public class Josephu {
 
     public static void main(String[] args) {
 
@@ -16,6 +16,9 @@ public class Josepfu {
 
         //遍历
         linkedList.list();
+
+        //测试出圈
+        linkedList.countBoy(1,2,5); //2 4 1 5 3
 
     }
 }
@@ -86,6 +89,69 @@ class SingleCircleLinkedList {
             //继续遍历
             curBoy = curBoy.next;
         }
+
+    }
+
+    //计算出圈的顺序
+
+    /**
+     * @param startNo  表示从第几个开始数
+     * @param countNum 表示数几下
+     * @param nums     表示最初有多少在圈
+     */
+    public void countBoy(int startNo, int countNum, int nums) {
+        //先对数据进行校验
+        if (first == null || startNo < 1 || startNo > nums || nums < 1) {
+            System.out.println("参数输入有误,请重新输入...");
+            return;
+        }
+
+        //日常辅助节点
+        Boy helper = first;
+
+        //1.给helper节点找最后一个节点
+        while (true) {
+
+            //helper指向了最后节点,找到了
+            if (helper.next == first) {
+                break;
+            }
+            //如果没有到最后一个节点,继续向下找
+            helper = helper.next;
+        }
+
+        //2.报数前,先让first和 helper,移动 startNo-1次,到达要开始数的位置
+        for (int i = 0; i < startNo -1; i++) {
+            first = first.next;
+            helper = helper.next;
+        }
+
+        //3.开始报数,first和 helper都移动 countNum-1次, 到达要出圈的位置
+        //4.然后出圈
+        while (true){
+
+            //判断圈里是否剩下最后一人
+            if (helper == first){
+                break;
+            }
+
+            //开始数,数到出圈的节点
+            for (int i = 0; i < countNum -1; i++) {
+                first = first.next;
+                helper = helper.next;
+            }
+
+            //出圈
+            System.out.printf("%d 号出圈\n",first.no);
+
+            //删除出圈节点
+            first = first.next;
+            helper.next = first;
+
+        }
+
+        //最后一个节点
+        System.out.printf("%d 号最后一个节点\n", helper.no);
 
     }
 
