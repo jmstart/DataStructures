ba3e4bb65ea3ad0b5d4179e8dd58924ee8fab4ae
tom.glb <goodMorning_glb@jiaming.com>
2020-03-21 13:37:21 +0800

    Google上机题

---
+++ DataStructures/src/com/jiaming/hashtab/GoogleHashTab.java
@@ -0,0 +1,112 @@
+package com.jiaming.hashtab;
+
+/**
+ * @author jmstart
+ * @create 2020-03-21 12:30
+ * 哈希表实现Google上机题:
+ * 有一个公司,当有新的员工来报道时,要求将该员工的信息加入(id,性别,年龄,住址..),
+ * 当输入该员工的id时,要求查找到该员工的 所有信息.
+ * 要求: 不使用数据库,尽量节省内存,速度越快越好 暗示使用=>哈希表(散列)
+ */
+public class GoogleHashTab {
+    public static void main(String[] args) {
+
+    }
+}
+
+//HashTab类
+class HashTab{
+    //放员工链表的数组,也就是哈希表
+    private EmpLinkedList[] empLinkedListArrays;
+    private int size; //表示有几条链表
+
+    //定义一个构造器
+    public HashTab(int size){
+        this.size = size;
+        //代表这个哈希表里有几个链表
+        empLinkedListArrays = new EmpLinkedList[size];
+    }
+
+    //添加员工
+    public void add(Emp emp){
+        //根据散列函数,得到员工要去的链表编号
+        int empLinkedListNO = hashFun(emp.id);
+
+        //添加对应链表
+        empLinkedListArrays[empLinkedListNO].addEmp(emp);
+    }
+
+    //编写散列函数,该函数方式有很多,我采用取模法
+    public int hashFun(int id) {
+        return id % size;
+    }
+
+    //遍历所有链表,也就是遍历哈希表
+    public void list(){
+        for (int i = 0; i < size; i++) {
+            empLinkedListArrays[i].listEmp();
+        }
+    }
+
+}
+
+//员工类
+class Emp {
+    //为了方便演示,就给员工二个属性,id 和 name
+    public int id;
+    public String name;
+    //指向下一个员工引用
+    public Emp next; //默认为空
+
+    public Emp(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+//链表类 存放员工信息
+class EmpLinkedList {
+    //头指针,指向第一个员工,head指向第一个员工
+    private Emp head; //默认为空
+
+    //添加员工
+    public void addEmp(Emp emp) {
+        //如果是添加第一个员工
+        if (head == null) {
+            head = emp;
+            return;
+        }
+        //如果不是第一个,则需要一个辅助指针帮我定位到链表最后
+        Emp curEmp = head;
+        while (true) {
+            //已经找到链表最后
+            if (curEmp.next == null) {
+                break;
+            }
+            //向下查找
+            curEmp = curEmp.next;
+        }
+        //添加员工
+        curEmp.next = emp;
+    }
+
+    //遍历链表员工信息
+    public void listEmp() {
+        //判断链表是否为空
+        if (head == null) {
+            System.out.println("当前链表为空...");
+            return;
+        }
+        System.out.println("当前链表的信息为:");
+        //日常辅助
+        Emp curEmp = head;
+        while (true){
+            System.out.printf(" => id=%d name=%s\t", curEmp.id, curEmp.name);
+            if (curEmp.next == null) {
+                break;
+            }
+            curEmp = curEmp.next;
+        }
+        System.out.println();
+    }
+}
