a665496065bf834f56c0f1824e68d21ab705d68e
tom.glb <goodMorning_glb@jiaming.com>
2020-03-22 15:27:17 +0800

    Google上机题

--- DataStructures/src/com/jiaming/hashtab/GoogleHashTab.java
+++ DataStructures/src/com/jiaming/hashtab/GoogleHashTab.java
@@ -1,5 +1,7 @@
 package com.jiaming.hashtab;
 
+import java.util.Scanner;
+
 /**
  * @author jmstart
  * @create 2020-03-21 12:30
@@ -10,25 +12,66 @@ package com.jiaming.hashtab;
  */
 public class GoogleHashTab {
     public static void main(String[] args) {
+        //创建哈希表
+        HashTab hashTab = new HashTab(7);
 
+        //菜单
+        String key = "";
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("add:  添加雇员");
+            System.out.println("list: 显示雇员");
+            System.out.println("find: 查找雇员");
+            System.out.println("exit: 退出系统");
+
+            key = scanner.next();
+            switch (key) {
+                case "add":
+                    System.out.println("输入id");
+                    int id = scanner.nextInt();
+                    System.out.println("输入名字");
+                    String name = scanner.next();
+                    //创建员工
+                    Emp emp = new Emp(id, name);
+                    hashTab.add(emp);
+                    break;
+                case "list":
+                    hashTab.list();
+                    break;
+                case "find":
+                    System.out.println("请输入要查找的id");
+                    id = scanner.nextInt();
+                    hashTab.findById(id);
+                    break;
+                case "exit":
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    break;
+            }
+        }
     }
 }
 
 //HashTab类
-class HashTab{
+class HashTab {
     //放员工链表的数组,也就是哈希表
     private EmpLinkedList[] empLinkedListArrays;
     private int size; //表示有几条链表
 
     //定义一个构造器
-    public HashTab(int size){
+    public HashTab(int size) {
         this.size = size;
         //代表这个哈希表里有几个链表
         empLinkedListArrays = new EmpLinkedList[size];
+        //注意: 要把哈希表表里面的每一个链表都初始化
+        for (int i = 0; i < size; i++) {
+            empLinkedListArrays[i] = new EmpLinkedList();
+        }
     }
 
     //添加员工
-    public void add(Emp emp){
+    public void add(Emp emp) {
         //根据散列函数,得到员工要去的链表编号
         int empLinkedListNO = hashFun(emp.id);
 
@@ -42,9 +85,22 @@ class HashTab{
     }
 
     //遍历所有链表,也就是遍历哈希表
-    public void list(){
+    public void list() {
         for (int i = 0; i < size; i++) {
-            empLinkedListArrays[i].listEmp();
+            empLinkedListArrays[i].listEmp(i);
+        }
+    }
+
+    //根据 id查找员工
+    public void findById(int id) {
+        //根据散列函数,得出去哪个链表查找
+        int empLinkedListNO = hashFun(id);
+        Emp emp = empLinkedListArrays[empLinkedListNO].findEmpById(id);
+        //判断是否找到员工
+        if (emp != null) {
+            System.out.printf("在第%d条链表中,找到了id为%d的员工\n", (empLinkedListNO + 1), id);
+        } else {
+            System.out.println("在哈希表中,没有找到该员工...");
         }
     }
 
@@ -76,7 +132,7 @@ class EmpLinkedList {
             head = emp;
             return;
         }
-        //如果不是第一个,则需要一个辅助指针帮我定位到链表最后
+        //如果不是第一个,则需要一个辅助指针,帮我定位到链表最后
         Emp curEmp = head;
         while (true) {
             //已经找到链表最后
@@ -91,16 +147,16 @@ class EmpLinkedList {
     }
 
     //遍历链表员工信息
-    public void listEmp() {
+    public void listEmp(int no) {
         //判断链表是否为空
         if (head == null) {
-            System.out.println("当前链表为空...");
+            System.out.println("第 " + (no + 1) + " 条链表的信息为空...");
             return;
         }
-        System.out.println("当前链表的信息为:");
+        System.out.print("第 " + (no + 1) + " 条链表的信息为:");
         //日常辅助
         Emp curEmp = head;
-        while (true){
+        while (true) {
             System.out.printf(" => id=%d name=%s\t", curEmp.id, curEmp.name);
             if (curEmp.next == null) {
                 break;
@@ -108,5 +164,30 @@ class EmpLinkedList {
             curEmp = curEmp.next;
         }
         System.out.println();
+    }
+
+    //根据 id查找员工
+    public Emp findEmpById(int id) {
+        //判空
+        if (head == null) {
+            return null;
+        }
+        //日常辅助
+        Emp curEmp = head;
+        while (true) {
+            //找到员工
+            if (curEmp.id == id) {
+                break;
+            }
+            //退出
+            if (curEmp.next == null) {
+                curEmp = null; //置空
+                break;
+            }
+            //没有找到继续后移
+            curEmp = curEmp.next;
+        }
+        //返回员工
+        return curEmp;
     }
 }
